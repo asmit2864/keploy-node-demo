c27536086574ca8d2c46b6ba3732a0304c330229
Asmit Kumar <asmit2864@gmail.com>
2026-01-18 08:01:39 +0000

    Initial Node.js app for Keploy demo

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+// Simple API
+app.get("/sum", (req, res) => {
+  const a = Number(req.query.a);
+  const b = Number(req.query.b);
+
+  res.json({
+    result: a + b
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
